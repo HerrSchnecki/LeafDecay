91e20911d303f1d260fb3eeb1b9b33d6e0340ae2
FoxyMC <107026427+HerrSchnecki@users.noreply.github.com>
2025-05-20 17:13:03 +0100

    Nichts

---
+++ src/Herrschnecki/Leafdecay/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Herrschnecki\Leafdecay;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\LeavesDecayEvent;
+
+class Main extends PluginBase implements Listener {
+
+    private array $ignoredWorlds = [];
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->ignoredWorlds = $this->getConfig()->get("ignored-worlds", []);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onLeafDecay(LeavesDecayEvent $event): void {
+        $world = $event->getBlock()->getPosition()->getWorld()->getFolderName();
+        if (!in_array($world, $this->ignoredWorlds)) {
+            $event->cancel();
+        }
+    }
+}
